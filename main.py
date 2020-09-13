160c7eed18f1c49d68364e3fb0526b72e7fb998d
Abraao barros lacerda <abraaobarros3@gmail.com>
2020-09-13 17:58:18 -0300

    Remove CLI

--- main.py
+++ main.py
@@ -17,34 +17,5 @@ def main():
     core.run()
 
 
-def cli():
-    argv = sys.argv
-    orders = get_initial_orders()
-    parameters = CKParameters()
-    if argv.index('--interval_orders') != -1:
-        parameters.INTERVAL_ORDERS = float(
-            argv[argv.index('--interval_orders')+1])
-
-    if argv.index('--max_courier_time') != -1:
-        parameters.MAX_COURIER_TIME = float(
-            argv[argv.index('--max_courier_time')+1])
-
-    if argv.index('--min_courier_time') != -1:
-        parameters.MIN_COURIER_TIME = float(
-            argv[argv.index('--min_courier_time')+1])
-
-    if argv.index('--overflow_decay_modifier') != -1:
-        parameters.OVERFLOW_DECAY_MODIFIER = float(
-            argv[argv.index('--overflow_decay_modifier')+1])
-
-    if argv.index('--normal_decay') != -1:
-        parameters.NORMAL_DECAY_MODIFIER = float(
-            argv[argv.index('--normal_decay')+1])
-
-    core = CKitchen(orders, parameters=parameters)
-    core.simulate()
-    core.run()
-
-
 if __name__ == '__main__':
     main()
